c471939504d880b10b877715231614a4edf031e2
Konstantinos Mousoulis <konmous04@gmail.com>
2026-07-24 01:33:46 +0300

    Initial Commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_pharma_guide/screens/authenticate/authenticate.dart';
+import 'package:my_pharma_guide/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //return either Home or Authenticate widget
+    return Authenticate();
+  }
+}
