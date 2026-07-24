ed5a712603e9f14f928555eb03128f8cdede01da
Konstantinos Mousoulis <konmous04@gmail.com>
2026-07-24 21:26:58 +0300

    sign in page decoration

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_pharma_guide/services/auth.dart';
+import 'package:my_pharma_guide/shared/constants.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -15,27 +16,114 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white12,
-      appBar: AppBar(
-        backgroundColor: Colors.white12,
-        elevation: 0.0,
-        title: Text('Sign In'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null){
-              print('error signing in');
-            } else{
-              print('signed in');
-              print(result);
-            }
-          },
-          child: Text('Sign in anon'),
+      backgroundColor: Colors.grey[400],
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 130.0,),
+            Text(
+              'My Pharma Guide',
+              style: TextStyle(
+                fontSize: 25.0,
+                letterSpacing: 1.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'email'),
+                    onChanged: (val) {
+
+                    },
+                  ),
+                  SizedBox(height: 22.0,),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'password'),
+                    onChanged: (val) {
+
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 2.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Sign In'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20.0,),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Sign Up'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1.5,
+                      color: Colors.grey[200],
+                    ),
+                  ),
+                  SizedBox(width: 10.0,),
+                  Text('or'),
+                  SizedBox(width: 10.0,),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1.5,
+                      color: Colors.grey[200],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () async{
+                dynamic result = await _auth.signInAnon();
+                if (result == null){
+                  print('error');
+                } else{
+                  print('signed in');
+                  print(result);
+                }
+              },
+              child: Text(
+                'Continue Anonymously',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey[800],
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.grey[800],
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final textInputDecoration = InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  isDense: true,
+  contentPadding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 15.0),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15.0),
+    borderSide: const BorderSide(color: Colors.white, width: 2.0),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15.0),
+    borderSide: const BorderSide(color: Colors.white, width: 2.0),
+  ),
+);
